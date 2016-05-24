3c0116d5cd39a1fd5bdfca9787271545e8a34212
Cordaz <cordaz666@gmail.com>
2016-05-24 16:44:42 +0200

    Recommit: spectrum.c not committed

--- Spectrum/spectrum.c
+++ Spectrum/spectrum.c
@@ -2,126 +2,220 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define SPECTRUM_SIZE 262144 // = (4^10) / 4 for now assuming l=10 fixed
-
 #define A 0 //00
 #define C 1 //01
 #define G 2 //10
 #define T 3 //11
 
 #define INTSIZE 16 //Compiler dependet... Fixed size? For now assume 16 bits
+#define PARAM 4 //l, m, input file, output file
 
 #ifdef SILENT
 #define printf(...)
 #endif
 
+void rm_newline(char *);
+
+/* Compute the spectrum T_m,l
+ * 
+ */
 int main (int argc, char * argv[]) {
-   int l=10; //lenght of l-tuple
-   int m=6; //multiplicity
-   int i;
+   int l; //lenght of l-tuple
+   int m; //multiplicity
+   int i, j;
+   unsigned int spectrum_size = 262144; // = (4^10) / 4 for now assuming l=10 fixed
+   
+   //Read parameters
+   if(argc < PARAM+1) {
+      fprintf(stdout, "Error: parameters\n");
+      exit(1);
+   }
+   
+   l = atoi(argv[1]);
+   m = atoi(argv[2]);
+   char * inputFile;
+   if(!(inputFile = (char *)malloc(sizeof(char) * strlen(argv[3])))) {
+      printf("Error: allocation\n");
+      exit(1);
+   }
+   strcpy(inputFile, argv[3]);
+   
+   char * outputFile;
+   if(!(outputFile = (char *)malloc(sizeof(char) * strlen(argv[4])))) {
+      printf("Error: allocation\n");
+      exit(1);
+   }
+   strcpy(outputFile, argv[4]);
    
   /* Counter of occurences
    * The sequence is represented encoded in 2 bits for each character
    * So, from the address of the array cell and the frame (1,2,3 or 4)
    * we can find the original sequence.
    */
-   unsigned int counter[SPECTRUM_SIZE];
+   int16_t * counter;
+   unsigned long int arrayIndex;
+   if((!(counter = (int16_t *)malloc(sizeof(int16_t) * spectrum_size)))) {
+      printf("Error: allocation\n");
+      exit(1);
+   }
+   for(arrayIndex=0; arrayIndex<spectrum_size; arrayIndex++) {
+      counter[arrayIndex] = 0;
+   }
    
    //Addressing the counter
-   unsigned long int arrayIndex;
    unsigned short int frame;
    unsigned long int adder;
-   int count;
+   int16_t count;
    char * tuple;
-   if(!(tuple = (char *)malloc(sizeof(char) * l))) {
+   if(!(tuple = (char *)malloc(sizeof(char) * (l+1)))) {
       fprintf(stdout, "Error allocation\n");
       exit(1);
    }
    
-   //Should read tuple from the file and iterate
+   //Iterate over input file
+   FILE * in;
+   in = fopen(inputFile, "r");
    
-   //Tokenize touple
-   arrayIndex = 0;
-   frame = 0;
-   for(i=0; i<l-1; i++) {
-      switch(tuple[i]) {
+   fgets(tuple, l+1, in);
+   while (!feof(in)) {
+      rm_newline(tuple);
+      //Tokenize touple
+      arrayIndex = 0;
+      frame = 0;
+      for(i=0; i<l-1; i++) {
+         switch(tuple[i]) {
+            case 'A':
+               //Should add A (0)
+               break;
+            case 'C':
+               arrayIndex += C;
+               break;
+            case 'G':
+               arrayIndex += G;
+               break;
+            case 'T':
+               arrayIndex += T;
+               break;
+         }
+         if(i != l-2)
+            arrayIndex = arrayIndex << 2; //Move bit to left (if not last)
+      }
+      //The last one indexes the frame
+      switch(tuple[l-1]) {
          case 'A':
-            //Should add A (0)
+            frame=A * 4;
             break;
          case 'C':
-            arrayIndex += C;
+            frame=C * 4;
             break;
          case 'G':
-            arrayIndex += G;
+            frame=G * 4;
             break;
          case 'T':
-            arrayIndex += T;
+            frame=T * 4;
             break;
       }
-      arrayIndex = arrayIndex << 2; //Move bit to left
-   }
-   switch(tuple[l-1]) {
-      case 'A':
-         frame=A * 4;
-         break;
-      case 'C':
-         frame=C * 4;
-         break;
-      case 'G':
-         frame=G * 4;
-         break;
-      case 'T':
-         frame=T * 4;
-         break;
-   }
-   
-   //Counter update procedure
-   //Check if necessary (< m)
-   count = counter[arrayIndex];
-   count = count << (INTSIZE - frame);
-   count = count >> INTSIZE;
-   //Now count contains the wanted count
-   if(count < m) {
-      //Preparing adder
-      adder=1;
-      adder = adder << frame; //Move the 1 of multiple of 4 position in the correct frame
-      counter[arrayIndex] += adder; //Add one;
+      printf("Index: %lu, Frame: %u", arrayIndex, frame/4);
+      //Counter update procedure
+      //Check if necessary (< m)
+      //Can't exceed 4 bits
+      count = counter[arrayIndex];
+      count = count << (INTSIZE - frame -4);
+      count = count >> (INTSIZE - 4);
+      //Now count contains the wanted count
+      if(count < m) {
+         //Preparing adder
+         adder=1;
+         adder = adder << frame; //Move the 1 of multiple of 4 position in the correct frame
+         counter[arrayIndex] += adder; //Add one;
+      }
+      printf(", Count: %d\n", count);
+      //tuple read, read next
+      fgets(tuple, l+1, in);
    }
    
+   fclose(in);
+   free(inputFile);
    //End of counting section
    
-   //Start of writing output section
-   //Need to reconstruct the counted sequence from the encoding
+   /*
+   //DEBUG: print count
+   for(arrayIndex=0; arrayIndex<spectrum_size; arrayIndex++) {
+      for(j=0; j<4; j++) {
+         count = counter[arrayIndex];
+         count = count << (INTSIZE - (j*4) - 4);
+         count = count >> (INTSIZE - 4);
+         fprintf(stdout, "%3d", count);
+      }
+      fprintf(stdout, "\n");
+   }
+   */
+   
+   
+   FILE * out;
+   out = fopen(outputFile, "w");
    unsigned long int tmp;
-   for(arrayIndex=0; arrayIndex < SPECTRUM_SIZE; arrayIndex++) {
-      count = counter[arrayIndex];
-      count = count << (INTSIZE - frame);
-      count = count >> INTSIZE;
-      if(count >= m) {
-         for(i=0;i<l;i++) {
-            tmp = arrayIndex >> (l*2 - i*2 - 2);
-            switch(tmp) {
+   //Start of writing output section
+   for(arrayIndex=0; arrayIndex < spectrum_size; arrayIndex++) {
+      //For each index 4 sequence must be extracted
+      for(i=0;i<l-1;i++) {
+         tmp = arrayIndex << (i*2);
+         tmp = tmp >> (l*2 - i*2 - 2);
+         switch(tmp) {
+            case A:
+               tuple[i] = 'A';
+               break;
+            case C:
+               tuple[i] = 'C';
+               break;
+            case G:
+               tuple[i] = 'G';
+               break;
+            case T:
+               tuple[i] = 'T';
+               break;
+         }
+      }
+      tuple[l] = '\0'; //Terminate string
+      for(j=0; j<4; j++) {
+         count = counter[arrayIndex];
+         count = count << (INTSIZE - (j*4) - 4);
+         count = count >> (INTSIZE - 4);
+         if(count >= m) {
+            //Need to reconstruct the counted sequence from the encoding
+            switch(j) {
                case A:
-                  tuple[i] = 'A';
+                  tuple[l-1] = 'A';
                   break;
                case C:
-                  tuple[i] = 'C';
+                  tuple[l-1] = 'C';
                   break;
                case G:
-                  tuple[i] = 'G';
+                  tuple[l-1] = 'G';
                   break;
                case T:
-                  tuple[i] = 'T';
+                  tuple[l-1] = 'T';
                   break;
             }
+            //tuple now is the reconstructed sequence
+            
+            //Ready to write out
+            fprintf(out, "%s\n", tuple);
          }
       }
    }
-   //Here tuple contains the original sequence
-   //Transform in a string
-   tuple[l] = '\0';
+   fclose(out);
+   free(tuple);
+   free(counter);
+   free(outputFile);
    
-   //Ready to write out
    
    return 0;
+}
+
+void rm_newline(char * str) {
+   int i;
+   for(i=0; i<strlen(str) && str[i] != '\n'; i++)
+      ;
+   str[i] = '\0';
 }

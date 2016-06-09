bf81d0b08887462bf21a6c611342a4cf7419629f
Cordaz <cordaz666@gmail.com>
2016-06-09 18:17:34 +0200

    Fixed cuda_main.cu, new version of allocation and more data for thread added - Removed old spectrum_main.c

--- cuda_main.cu
+++ cuda_main.cu
@@ -1,14 +1,22 @@
+//#ifndef __CUDACC__
+//#define __CUDACC__
+//#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
-#include <cuda.h>
+//#include <cuda.h>
 #include <stdint.h>
-#include <device_functions.h>
+//#include <device_functions.h>
+//#include <cuda_runtime_api.h>
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
 
 #define PARAM 5 //spectrum (bloom filter), dim, input, dim, output
 #define BASES 4
 #define BLOCK_DIM 16
-#define READS_LENGTH 35
+#define DATA_PER_THREAD 10
+#define READS_LENGTH 34
 #define L 10
 
 /************ ERROR HANDLING *****************************/
@@ -30,8 +38,6 @@ void HandleError( cudaError_t err,
  * In order to reduce acces latency it will be stored on shared memory
  */
 __shared__ char bases[BASES];
-
-__constant__ uint64_t ** gpu_hashed_spectrum;
 
 /****************  DEVICE FUNCTIONS  *********************/
 __device__ ushort2 matrix_maximum(unsigned short int ** v) {
@@ -67,11 +73,11 @@ __device__ void gpu_strcpy(char * a, char * b) {
 
 /***************** FIXING KERNEL ***************************/
 
-__global__ void fixing(char ** reads, unsigned short int *** voting_matrix_array, unsigned int * gpu_inputDim, char ** dev_bases) {
+__global__ void fixing(char ** reads, unsigned short int *** voting_matrix_array, unsigned int inputDim, uint64_t ** gpu_hashed_spectrum) {
    ushort2 trim_indexes;
    trim_indexes.x = 0; //Starting index of longest substring
    trim_indexes.y = 0; //End index of longest substring
-   short int i;
+   short int i, h;
    
    int idx = blockIdx.x * blockDim.x + threadIdx.x;
    
@@ -80,78 +86,84 @@ __global__ void fixing(char ** reads, unsigned short int *** voting_matrix_array
     * 
     */
    if(threadIdx.x == 0) {
-      for(i=0; i<BASES; i++) {
-         bases[i] = *dev_bases[i];
-      }
+      bases[0] = 'A';
+      bases[1] = 'C';
+      bases[2] = 'G';
+      bases[3] = 'T';
    }
-   __synchthreads(); //TODO
-   
-   if(idx >= *gpu_inputDim)
-      return;
+   __syncthreads();
    
    char * read;
    unsigned short int ** voting_matrix;
-   
-   read = (char *)reads[idx];
-   voting_matrix = (unsigned short int **)voting_matrix_array[idx];
-   
-   ushort2 couple = matrix_maximum(voting_matrix);
-   
-   if(voting_matrix[couple.x][couple.y] == 0) {
-      return; //read is already correct
-   }
-   
    char rc[READS_LENGTH+1];
-   for(i=0; i<couple.x; i++) {
-      rc[i] = read[i];
-   }
-   rc[i] = bases[couple.y];
-   for(i=couple.x+1; i<READS_LENGTH; i++) {
-      rc[i] = read[i];
-   }
-   
-   bool corrected_flag=1, trimmed_flag=0;
    unsigned short int j;
    char tuple[L + 1];
+   bool corrected_flag, trimmed_flag;
+   ushort2 couple;
    
-   for(j=0;j < (READS_LENGTH-(L+1)); j++) {
-      //Create tuple
-      for(i=j; i<j + L; i++) {
-         tuple[i-j] = read[j];
+   for(h=0; h<DATA_PER_THREAD; h++) {
+      if(idx + inputDim/DATA_PER_THREAD * h >= inputDim)
+         return;
+      read = (char *)reads[idx + inputDim/DATA_PER_THREAD * h];
+      voting_matrix = (unsigned short int **)voting_matrix_array[idx];
+      
+      couple = matrix_maximum(voting_matrix);
+      
+      if(voting_matrix[couple.x][couple.y] == 0) {
+         continue; //read is already correct
       }
-      tuple[L] = '\0';
-      if( !(1/* TODO - query bloom filter for tuple */) ) {
-         corrected_flag = 0;
-        /* Check for trimming
-         * If current subsequence is longer than previous one then update
-         * Else the longest subsequent is already stored
-         */
-         if( (j+8 - trim_indexes.y+2) > (trim_indexes.y - trim_indexes.x) ) {
-            trim_indexes.x = trim_indexes.y+2;
-            trim_indexes.y = j + L - 1;
+      
+      for(i=0; i<couple.x; i++) {
+         rc[i] = read[i];
+      }
+      rc[i] = bases[couple.y];
+      for(i=couple.x+1; i<READS_LENGTH; i++) {
+         rc[i] = read[i];
+      }
+      
+      corrected_flag=1;
+      trimmed_flag=0;
+      
+      for(j=0;j < (READS_LENGTH-(L+1)); j++) {
+         //Create tuple
+         for(i=j; i<j + L; i++) {
+            tuple[i-j] = read[j];
+         }
+         tuple[L] = '\0';
+         if( !(1/* TODO - query bloom filter for tuple */) ) {
+            corrected_flag = 0;
+           /* Check for trimming
+            * If current subsequence is longer than previous one then update
+            * Else the longest subsequent is already stored
+            */
+            if( (j+8 - trim_indexes.y+2) > (trim_indexes.y - trim_indexes.x) ) {
+               trim_indexes.x = trim_indexes.y+2;
+               trim_indexes.y = j + L - 1;
+            }
+         }
+         else {
+            trimmed_flag = 1;
          }
       }
-      else {
-         trimmed_flag = 1;
+      
+      if(corrected_flag) {
+         gpu_strcpy(reads[idx + inputDim/DATA_PER_THREAD * h], rc); //Return corrected read
+         continue;
       }
-   }
-   
-   if(corrected_flag) {
-      gpu_strcpy(read, rc); //Return corrected read
-      return;
-   }
-   
-   if(trimmed_flag) {
-      //Trim read
-      for(i=trim_indexes.x, j=0; i<trim_indexes.y; i++, j++) {
-         read[j] = rc[i];
+      
+      if(trimmed_flag) {
+         //Trim read
+         for(i=trim_indexes.x, j=0; i<trim_indexes.y; i++, j++) {
+            read[j] = rc[i];
+         }
+         read[j] = '\0';
+         gpu_strcpy(reads[idx + inputDim/DATA_PER_THREAD * h], read);
+         continue;
       }
-      read[j] = '\0';
-      return;
+      
+      //Uncorrect read, return empty string
+      reads[idx + inputDim/DATA_PER_THREAD * h][0] = '\0';
    }
-   
-   //Uncorrect read, return empty string
-   read[0] = '\0';
 }
 
 
@@ -167,46 +179,29 @@ int main (int argc, char * argv[]) {
    }
    
    /************** INITIALIZATION **************/
-   
-   /* Allocate basis vector
-    * 
-    */
-   /* WARNING:
-    * cuda_main.cu(157): warning: address of a __shared__ variable "bases"
-    * cannot be directly taken in a host function
-    * 
-    * Also on lines 231, 237, 325 and 331
-    */
-   char host_bases[BASES] = {'A', 'C', 'G','T'};
-   char ** dev_bases;
-   HANDLE_ERROR(cudaMalloc((void **)&dev_bases, sizeof(char *) * BASES));
-   for(i=0; i<BASES; i++) {
-      HANDLE_ERROR(cudaMalloc((void **)&dev_bases[i], sizeof(char)));
-      HANDLE_ERROR(cudaMemcpy((void *)&dev_bases[i], (const void *)&host_bases[i], sizeof(char), cudaMemcpyHostToDevice));
-   }
 
    /* Allocate and store the spectrum on the host,
     * reading it from the input file
     * 
     */
    uint64_t * hashed_spectrum;
-   int spectrum_size; //TODO
+   int spectrum_size = atoi(argv[2]);
    
    if(!(hashed_spectrum = (uint64_t *)malloc(sizeof(uint64_t) * spectrum_size))) {
       fprintf(stdout, "Error: allocation\n");
       exit(1);
    }
    
-   FILE * spectrumFP = fopen(argv[2], "r");
+   FILE * spectrumFP = fopen(argv[1], "r");
    
    for(i=0; i<spectrum_size; i++) {
-      fscanf(spectrumFP, "%lu", &hashed_spectrum[i]); //TODO
-      printf("%x\n", hashed_spectrum[i]);
+      fscanf(spectrumFP, "%lu", &hashed_spectrum[i]);
+      printf("%lu\n", hashed_spectrum[i]);
    }
    
    fclose(spectrumFP);
    
-   const unsigned int inputDim = atoi(argv[5]);
+   const unsigned int inputDim = atoi(argv[4]);
    
    /* Allocate and read from input file the sequence reads,
     * on host memory
@@ -225,7 +220,7 @@ int main (int argc, char * argv[]) {
       }
    }
    
-   FILE * readsFP = fopen(argv[4], "r");
+   FILE * readsFP = fopen(argv[3], "r");
    
    for(i=0; i<inputDim; i++) {
       fgets(reads[i], READS_LENGTH+1, readsFP);
@@ -241,6 +236,7 @@ int main (int argc, char * argv[]) {
     * 
     * 
     */
+   uint64_t ** gpu_hashed_spectrum;
    HANDLE_ERROR(cudaMalloc((void **)&gpu_hashed_spectrum, sizeof(uint64_t *) * spectrum_size));
    for(i=0; i<spectrum_size; i++) {
       HANDLE_ERROR(cudaMalloc((void **)&gpu_hashed_spectrum[i], sizeof(uint64_t)));
@@ -260,15 +256,7 @@ int main (int argc, char * argv[]) {
       HANDLE_ERROR(cudaMemcpy(gpu_reads[i], reads[i], sizeof(char) * (READS_LENGTH + 1), cudaMemcpyHostToDevice));
    }
    
-   /* Allocate inputDim on gpu memory as gpu_inputDim
-    * Include memcopy
-    * 
-    */
-   unsigned int * gpu_inputDim;
-   HANDLE_ERROR(cudaMalloc((void **)&gpu_inputDim, sizeof(unsigned int)));
-   HANDLE_ERROR(cudaMemcpy(gpu_inputDim, &inputDim, sizeof(unsigned int), cudaMemcpyHostToDevice));
-   
-   /* Initialize voting_matrix of zeros
+   /* Initialize voting_matrix
     * Then proceed with the allocation on device memory of gpu_voting_matrix
     * Include memcopy of the initialized matrix
     * 
@@ -287,9 +275,6 @@ int main (int argc, char * argv[]) {
          if(!(voting_matrix[i][j] = (unsigned short int *)malloc(sizeof(unsigned short int) * BASES))) {
             fprintf(stdout, "Error: allocation\n");
             exit(1);
-         }
-         for(h=0; h<BASES; h++) {
-            voting_matrix[i][j][h] = 0;
          }
       }
    }
@@ -325,7 +310,7 @@ int main (int argc, char * argv[]) {
     */
    
    //Execute kernel
-   fixing <<< inputDim/BLOCK_DIM, BLOCK_DIM >>> (gpu_reads, gpu_voting_matrix, gpu_inputDim, dev_bases);
+   fixing <<< inputDim/BLOCK_DIM/DATA_PER_THREAD, BLOCK_DIM >>> (gpu_reads, gpu_voting_matrix, inputDim, gpu_hashed_spectrum);
    
    /************ RETRIEVE RESULT ********/
    
@@ -341,7 +326,6 @@ int main (int argc, char * argv[]) {
    /************* MEM FREE *****************/
    /* Only not freed yet */
    
-   cudaFree((void **)&gpu_inputDim);
    for(i=0; i<inputDim; i++) {
       cudaFree(gpu_reads[i]);
       free(reads[i]);
@@ -353,7 +337,7 @@ int main (int argc, char * argv[]) {
    /************* WRITE OUT RESULTS ********/
    
    FILE * outFP;
-   outFP = fopen(argv[6], "w");
+   outFP = fopen(argv[5], "w");
    
    for(i=0; i<inputDim; i++) {
       //Ignore discarded reads
